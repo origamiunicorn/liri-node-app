1689ac6153ec37000846426e2d58104282c65590
origami unicorn <rvwright@gmail.com>
2019-12-19 17:15:25 -0800

    After struggling with the Spotify API NPM, went to Spotify directly and used their GitHub project to not use an NPM and instead create my own token using the client id and client secret generated for my project via Spodity Developer's app registration. Successfully tested in separate js, then moved and implemented in liri.js. Added in the default search value for when no song title is provided, and changed the search URL so that the results returned are the song required by the project.

--- liri.js
+++ liri.js
@@ -1,53 +1,41 @@
 require("dotenv").config();
 
+const keys = require("./keys.js");
 const axios = require("axios");
 const moment = require('moment');
 const fs = require("fs");
-const keys = require("./keys.js");
+var request = require('request');
 
-const [node, file, action, ...args] = process.argv;
-console.log(node);
-console.log(file);
-console.log(action);
-console.log(args);
+var client_id = keys.spotify.id;
+var client_secret = keys.spotify.secret;
 
-const searchVariable = args.join(" ");
-const actionTaken = action;
-
-// var spotify = new Spotify(keys.spotify);
-
-// if (action === "concert-this") {
-//     console.log(`${searchVariable}'s upcoming events.
-//     `);
-//     searchBandsInTown(searchVariable);
-// } else if (action === "spotify-this-song") {
-//     console.log(`Spotify this song, "${process.argv[3]}."`);
-// } else if (action === "movie-this") {
-//     console.log(`Search for information on the movie titled ${process.argv[3]}.`);
-// } else if (action === "do-what-it-says") {
-//     console.log(`Do what it says.`);
-// } else {
-//     console.log(`Please use one of the four available commands. They are as follows:
-//     concert-this <artist>
-//     spotify-this-song <song title>
-//     movie-this <movie title>
-//     do-what-it-says`)
-// };
+let [node, file, action, ...args] = process.argv;
+let searchVariable = args.join(" ");
+let actionTaken = action;
 
 switch (actionTaken) {
     case "concert-this":
-        console.log(`${searchVariable}'s upcoming events.
-`);
         searchBandsInTown(searchVariable);
         break;
     case "spotify-this-song":
-        console.log(`Spotify this song, "${searchVariable}."`);
+        if (searchVariable !== "") {
+            console.log(`Searching Spotify for a song called, "${searchVariable}."`);
+            searchVariable.split(" ").join("+");
+            searchSpotifyAPI(searchVariable);
+        } else {
+            searchVariable = "track:the+sign+artist:ace+of+base";
+            console.log(`Searching Spotify for a song called, "${searchVariable}."`);
+            searchSpotifyAPI(searchVariable);
+        }
         break;
     case "movie-this":
-        console.log(`Search for information on the movie titled ${searchVariable}.`);
+        console.log(`Searching for information on a movie titled ${searchVariable}.`);
+        searchVariable.split(" ").join("+");
+        searchMovies(searchVariable, "");
         break;
     case "do-what-it-says":
         console.log(`Do what it says.`);
+        doTheThing();
         break;
     default:
         console.log(`Please use one of the four available commands. They are as follows:
@@ -56,7 +44,106 @@ switch (actionTaken) {
   movie-this <movie title>
   do-what-it-says`)
         break;
+};
+
+function doTheThing() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        if (error) {
+            return console.log(error);
+        }
+
+        var dataArr = data.split(",");
+        actionTaken = dataArr[0];
+        searchVariable = dataArr[1];
+
+        switch (actionTaken) {
+            case "concert-this":
+                searchBandsInTown(searchVariable);
+                break;
+            case "spotify-this-song":
+                console.log(`Spotify this song, "${searchVariable}."`);
+                searchSpotifyAPI();
+                break;
+            case "movie-this":
+                console.log(`Searching for information on a movie titled ${searchVariable}.`);
+                searchVariable.split(" ").join("+");
+                searchMovies(searchVariable, "");
+                break;
+        };
+    });
+};
+
+function searchMovies(movie = "Mr. Nobody") {
+
+    let queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=$&plot=short&apikey=" + keys.omdb.id;
+
+    axios
+        .get(queryURL)
+        .then(
+            function (response) {
+                // console.log(response)
+                console.log(`
+Title: ${response.data.Title}
+Release Year: ${response.data.Year}
+IMDB Rating: ${response.data.imdbRating}
+Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}
+Produced In: ${response.data.Country}
+Movie Language(s): ${response.data.Language}
+Plot Synopsis: ${response.data.Plot}
+Top Billing Actors: ${response.data.Actors}`);
+            })
+        .catch(
+            function (error) {
+                if (error.response) {
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    console.log(error.request);
+                } else {
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+            });
 }
+
+function searchSpotifyAPI(song = "The Sign") {
+    var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+            'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+        },
+        form: {
+            grant_type: 'client_credentials'
+        },
+        json: true
+    };
+
+    request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+
+            var token = body.access_token;
+            var options = {
+                url: 'https://api.spotify.com/v1/search?q=' + song + '&offset=0&limit=5&type=track',
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                },
+                json: true
+            };
+            request.get(options, function (error, response, body) {
+
+                for (let i = 0; i < 5; i++) {
+                    console.log(`
+Song: ${body.tracks.items[i].name}
+Artist: ${body.tracks.items[i].artists[0].name}
+Album: ${body.tracks.items[i].album.name}
+Preview URL: ${body.tracks.items[i].preview_url}`);
+                }
+            });
+        }
+    });
+};
 
 function searchBandsInTown(artist) {
 
@@ -67,17 +154,18 @@ function searchBandsInTown(artist) {
         .then(function (response) {
             // console.log(response);
             if (response.data.length === 0) {
-                console.log(`There are no upcoming shows by ${artist} visible on Bands In Town.`)
+                console.log(`
+There are no upcoming shows by ${artist} visible on Bands In Town.`)
             } else {
-                console.log(`Upcoming shows by ${artist}:
-`)
+                console.log(`
+Upcoming shows by ${artist}:`)
             }
 
             for (let i = 0; i < response.data.length; i++) {
-                console.log(`Venue: ${response.data[i].venue.name}
+                console.log(`
+Venue: ${response.data[i].venue.name}
 Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}
-Date: ${moment(response.data[i].datetime).format("MM/DD/YYYY")}
-`);
+Date: ${moment(response.data[i].datetime).format("MM/DD/YYYY")}`);
             }
 
         })
@@ -94,4 +182,3 @@ Date: ${moment(response.data[i].datetime).format("MM/DD/YYYY")}
             console.log(error.config);
         });
 };
-
